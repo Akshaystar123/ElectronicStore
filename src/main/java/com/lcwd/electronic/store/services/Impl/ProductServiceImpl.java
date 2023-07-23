aa72374406c64fb877e8cb76d4ba3b670891651e
Akshaystar123 <khaireakshay229@gmail.com>
2023-07-23 12:16:52 +0530

    ProductServiceImpl logger changes successfully

--- src/main/java/com/lcwd/electronic/store/services/Impl/ProductServiceImpl.java
+++ src/main/java/com/lcwd/electronic/store/services/Impl/ProductServiceImpl.java
@@ -47,20 +47,20 @@ public class ProductServiceImpl implements ProductServiceI {
 
     @Override
     public ProductDto createProduct(ProductDto productDto) {
-        log.info("Sending dao call to create product");
+        log.info("Sending dao call to create product:{}",productDto.getTitle());
         Product product = modelMapper.map(productDto, Product.class);
         String productId = UUID.randomUUID().toString();
         product.setProductId(productId);
         Product saveProduct = productRepositoryI.save(product);
         ProductDto productDto1 = modelMapper.map(saveProduct, ProductDto.class);
-        log.info("Completed dao call to successful create product");
+        log.info("Completed dao call to successful create product:{}",productDto.getTitle());
         return productDto1;
     }
 
     @Override
     public ProductDto updateProduct(ProductDto productDto, String productId) {
 
-        log.info("Sending dao call to update product");
+        log.info("Sending dao call to update product:{}",productId);
         Product product = productRepositoryI.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.PRODUCT_NOT_FOUND + productId));
         product.setTitle(productDto.getTitle());
         product.setQuantity(productDto.getQuantity());
@@ -73,32 +73,33 @@ public class ProductServiceImpl implements ProductServiceI {
         product.setDiscountPrice(productDto.getDiscountPrice());
         Product updatedProduct = productRepositoryI.save(product);
         ProductDto updatedProductDto = modelMapper.map(updatedProduct, ProductDto.class);
-        log.info("Completed dao call to successful update product");
+        log.info("Completed dao call to successful update product:{}",productId);
         return updatedProductDto;
     }
 
     @Override
     public void deleteProduct(String productId) {
-        log.info("Sending dao call to delete product");
+        log.info("Sending dao call to delete product:{}",productId);
         Product product = productRepositoryI.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.PRODUCT_NOT_FOUND));
         String fullPath = imageFullPath + product.getProductImage();
         try {
             Path path = Paths.get(fullPath);
             Files.delete(path);
         } catch (NoSuchFileException ex) {
+            log.info("No such file found");
             ex.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         productRepositoryI.delete(product);
-        log.info("Completed dao call to successful delete product");
+        log.info("Completed dao call to successful delete product{}",productId);
     }
 
     @Override
     public PageableResponse<ProductDto> getAllProducts(int pageNumber, int pageSize, String sortBy, String sortDir) {
         log.info("Sending dao call to get all product");
-        Sort sort = (sortBy.equalsIgnoreCase("asc")) ? (Sort.by(sortBy).ascending()) : (Sort.by(sortBy).ascending());
-        Pageable pageable = PageRequest.of(pageSize, pageNumber, sort);
+        Sort sort = (sortBy.equalsIgnoreCase("asc"))?(Sort.by(sortDir).ascending()):(Sort.by(sortDir).descending());
+        Pageable pageable = PageRequest.of(pageNumber,pageSize,sort);
         Page<Product> page = productRepositoryI.findAll(pageable);
         PageableResponse<ProductDto> pageableResponse = Helper.getPageableResponse(page, ProductDto.class);
         log.info("Completed dao call to successful get all product");
@@ -107,25 +108,25 @@ public class ProductServiceImpl implements ProductServiceI {
 
     @Override
     public ProductDto getSingleProduct(String productId) {
-        log.info("Sending dao call to get single product");
+        log.info("Sending dao call to get single product:{}",productId);
         Product singleProduct = productRepositoryI.findById(productId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.PRODUCT_NOT_FOUND));
         ProductDto singleProductDto = modelMapper.map(singleProduct, ProductDto.class);
-        log.info("Completed dao call to successful get single product");
+        log.info("Completed dao call to successful get single product:{}",productId);
         return singleProductDto;
     }
 
     @Override
     public ProductDto searchProduct(String keyword) {
-        log.info("Sending dao call to get product keyword");
+        log.info("Sending dao call to get product keyword:{}",keyword);
         List<ProductDto> ProductByTitleContaining = productRepositoryI.findByTitleContaining(keyword);
         ProductDto ProductDtoByTitleContaining = modelMapper.map(ProductByTitleContaining, ProductDto.class);
-        log.info("Completed dao call to successful get product keyword");
+        log.info("Completed dao call to successful get product keyword:{}",keyword);
         return ProductDtoByTitleContaining;
     }
 
     @Override
     public ProductDto createProductWithCategory(ProductDto productDto, String categoryId) {
-        log.info("Sending dao call to get product with category");
+        log.info("Sending dao call to get product with category:{}",categoryId);
         Category category = categoryrepositoryI.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException(AppConstants.CATEGORY_NOT_FOUND + categoryId));
         Product product = modelMapper.map(productDto, Product.class);
         String productId = UUID.randomUUID().toString();
@@ -134,7 +135,7 @@ public class ProductServiceImpl implements ProductServiceI {
         product.setCategory(category);
         Product savedProduct = productRepositoryI.save(product);
         ProductDto savedProductDto = modelMapper.map(savedProduct, ProductDto.class);
-        log.info("Completed dao call to successful get product keyword");
+        log.info("Completed dao call to successful get product with category:{}",categoryId);
         return savedProductDto;
     }
 }
